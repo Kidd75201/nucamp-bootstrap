1cc76bd2a07dd9b4d6a5cafef00ed485303133cd
Kidd75201 <jbezal@gmail.com>
2020-12-29 23:22:39 -0600

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function () {
+    // slide time
+    $(".carousel").carousel({ interval: 2000 });
+
+    // button Pause/Play
+    $("#carouselButton").click(function () {
+      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+        $(".carousel").carousel("pause");
+        $("#carouselButton").children("i").removeClass("fa-pause");
+        $("#carouselButton").children("i").addClass("fa-play");
+        //alert(".click() if function .carousel(\"pause\"); called.");
+      } else {
+        $(".carousel").carousel("cycle");
+        $("#carouselButton").children("i").removeClass("fa-play");
+        $("#carouselButton").children("i").addClass("fa-pause");
+        //alert(".click() else function .carousel(\"cycle\") called.");
+      }
+    });
+    
+  });
